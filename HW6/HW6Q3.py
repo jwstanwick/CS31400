efe09b68476f86fccbaf6fe580913e537bf64886
John Stanwick <jwstanwick@gmail.com>
2023-11-16 14:21:05 -0500

    HWQ3 Solved

--- HW6/HW6Q3.py
+++ HW6/HW6Q3.py
@@ -1,18 +1,96 @@
-x_val = [-5, -4.6552, -4.3103, -3.9655, -3.6207, 
+import numpy as np
+from math import *
+from HelperFunctions import *
+
+x_arr = np.array([-5, -4.6552, -4.3103, -3.9655, -3.6207, 
          -3.2759, -2.931, -2.5862, -2.2414, -1.8966, 
          -1.5517, -1.2069, -0.86207, -0.51724, -0.17241, 
          0.17241, 0.51724, 0.86207, 1.2069, 1.5517, 1.8966, 
          2.2414, 2.5862, 2.931, 3.2759, 3.6207, 3.9655, 4.3103, 
-         4.6552, 5]
+         4.6552, 5])
 
-y_val = [25.198, 22.354, 11.286, -5.3643, -9.9406, 5.5574, 20.364, 
+y_arr = np.array([25.198, 22.354, 11.286, -5.3643, -9.9406, 5.5574, 20.364, 
          10.826, 12.018, 18.742, -4.5468, -6.2935, 11.038, -6.9372, 
          22.685, 10.281, -4.0523, -0.26773,10.089, 13.64, 20.074,
          13.906, 2.7136, 16.384, 2.9209, 27.113, 30.377, 29.667,
-         22.724, 48.731]
+         22.724, 48.731])
+
+# Implement a program to derive the least squares quadratic polynomial by solving the normal equations. State the limitation of this approach.
+
+def Q3a(x_arr, y_arr):
+    sum_x4 = np.sum(x_arr ** 4)
+    sum_x3 = np.sum(x_arr ** 3)
+    sum_x2 = np.sum(x_arr ** 2)
+    sum_x = np.sum(x_arr)
+    sum_x2y = np.sum(x_arr ** 2 * y_arr)
+    sum_xy = np.sum(x_arr * y_arr)
+    sum_y = np.sum(y_arr)
+    
+    A = np.array([
+        [sum_x4, sum_x3, sum_x2], 
+        [sum_x3, sum_x2, sum_x], 
+        [sum_x2, sum_x, len(x_arr)]
+    ])
+    
+    b = np.array([sum_x2y, sum_xy, sum_y]) 
+    
+    # Whoops, not allowed to use .linalg.solve!
+    # polynomial = np.linalg.solve(A, b)
+    polynomial = invert3x3(A.T @ A) @ A.T @ b
+    
+    return polynomial
+
+# Implement a program to derive the least square quadratic polynomial using QR factorization that finds the orthogonal projection of b onto the column space of A.
+def Q3b(x_arr, y_arr):
+    sum_x4 = np.sum(x_arr ** 4)
+    sum_x3 = np.sum(x_arr ** 3)
+    sum_x2 = np.sum(x_arr ** 2)
+    sum_x = np.sum(x_arr)
+    sum_x2y = np.sum(x_arr ** 2 * y_arr)
+    sum_xy = np.sum(x_arr * y_arr)
+    sum_y = np.sum(y_arr)
+    
+    A = np.array([
+        [sum_x4, sum_x3, sum_x2], 
+        [sum_x3, sum_x2, sum_x], 
+        [sum_x2, sum_x, len(x_arr)]
+    ])
+    b = np.array([sum_x2y, sum_xy, sum_y]) 
+    
+    Q, R = generateQRFactors(A)
+    Q = np.array(Q)
+    R = np.array(R)
+    
+    QTB = Q.T @ b
+    
+    polynomial = backSubstitution(R, QTB)
+    return polynomial
 
 def main():
-    return
+    PLOT = True
+    
+    Q3a_sol = Q3a(x_arr, y_arr)
+    Q3a_sol_p = [Q3a_sol[0] * x ** 2 + Q3a_sol[1] * x + Q3a_sol[2] for x in x_arr]    
+    if PLOT:
+        plt.plot(x_arr, y_arr, x_arr, Q3a_sol_p)
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.title("Q3a")
+        plt.show()
+        
+    print("The limitation to this approach is that it becomes very inaccurate if the array of sums for x^4, x^3, etc. are not invertible or hard to determine accurately, then the polynomial will blow up. Futher, the polynomial will be very inaccurate if the determinant of A is very small or very large.")
+    
+    Q3b_sol = Q3b(x_arr, y_arr)
+    Q3b_sol_p = [Q3b_sol[0] * x ** 2 + Q3b_sol[1] * x + Q3b_sol[2] for x in x_arr]  
+    if PLOT:
+        plt.plot(x_arr, y_arr, x_arr, Q3b_sol_p)
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.title("Q3a")
+        plt.show()
+    
+    
+    
 
 if __name__=="__main__":
 	main()
--- HW6/HelperFunctions.py
+++ HW6/HelperFunctions.py
@@ -1,8 +1,9 @@
 from math import *
 import numpy as np
+import matplotlib.pyplot as plt
 
 def riseRun(x0, x1, y0, y1):
-    return (y1 - y0) / (x1 - x0)
+	return (y1 - y0) / (x1 - x0)
 
 def calcNewtonPolynomial(x_arr, y_arr, degree):
 	table = np.zeros((degree, degree))
@@ -14,9 +15,9 @@ def calcNewtonPolynomial(x_arr, y_arr, degree):
 	return table[0]
 	
 def printPolynomial(polynomial):
-    for i in range(len(polynomial)):
-        print(f"a{i}: {round(polynomial[i], 6)}")
-        
+	for i in range(len(polynomial)):
+		print(f"a{i}: {round(polynomial[i], 6)}")
+		
 def calcPolynomialVal(x, polynomial):
 	val = 0
 	for i in range(len(polynomial)):
@@ -40,4 +41,52 @@ def maxInterpolationError(x_arr, y_arr, p_arr):
 		if abs(error) > max_error:
 			max_error = abs(error)
 	return max_error
-        
+
+def plotPoints(x_arr, y_arr, title, x_label, y_label):
+	plt.plot(x_arr, y_arr)
+	plt.xlabel(x_label)
+	plt.ylabel(y_label)
+	plt.title(title)
+	plt.show()
+
+# This code is terrible. And makes me sad. 
+# I promise I originally tried to not brute force it.
+# But I have an exam tomorrow and I don't have time to make it better. Sorry prof!
+def invert3x3(A):
+	a, b, c, d, e, f, g, h, i = A[0][0], A[0][1], A[0][2], A[1][0], A[1][1], A[1][2], A[2][0], A[2][1], A[2][2]
+	det = a*e*i - a*f*h - b*d*i + b*f*g + c*d*h - c*e*g
+	return [[(e*i - f*h)/det, -(b*i - c*h)/det, (b*f - c*e)/det],
+			[-(d*i - f*g)/det, (a*i - c*g)/det, -(a*f - c*d)/det],
+			[(d*h - e*g)/det, -(a*h - b*g)/det, (a*e - b*d)/det]]
+
+# This QR Factors Code was created with help from:
+# - The Berkeley Numerical Methods online textbook - https://pythonnumericalmethods.berkeley.edu/notebooks/chapter15.03-The-QR-Method.html
+# - https://johnwlambert.github.io/least-squares/
+# - Extra help from ChatGPT
+def generateQRFactors(A):
+	n, m = len(A), len(A[0])
+	Q = [[0]*m for i in range(n)]
+	R = [[0]*m for i in range(m)]
+
+	for j in range(m):
+		for i in range(n):
+			Q[i][j] = A[i][j]
+		for k in range(j):
+			R[k][j] = sum(Q[i][j] * Q[i][k] for i in range(n))
+			for i in range(n):
+				Q[i][j] -= R[k][j] * Q[i][k]
+		R[j][j] = sqrt(sum(Q[i][j]**2 for i in range(n)))
+		for i in range(n):
+			Q[i][j] /= R[j][j]
+	return Q, R
+
+# Solving a back substitution problem for an upper triangular matrix U and a vector b
+def backSubstitution(U, b):
+	n = len(U)
+	x = np.zeros(n)
+	for i in range(n - 1, -1, -1):
+		x[i] = b[i]
+		for j in range(i + 1, n):
+			x[i] -= U[i][j] * x[j]
+		x[i] /= U[i][i]
+	return x
